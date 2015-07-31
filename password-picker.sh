b3c0b51c69da17fb4ec4075620e96110e24c20d0
Oliver Runge <oliver.runge@gmail.com>
2015-07-31 22:35:47 +0200

    Add scripts
    password-picker.sh uses keepass-client, but it could use any other
    command line scripts that can list all entries and return a password to
    stdout for a given entry.

---
+++ password-picker.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+database_file="$1"
+
+script_path=$(readlink "$0")
+if [ -z "$script_path" ]; then
+    script_path="$0"
+fi
+
+password_picker_dir=$(dirname "$script_path")
+password_prompt_command="zenity --password --text 'master password:' --title 'Master Password' 2>/dev/null"
+bad_password_alert="zenity --error --text 'bad password or no entries' --title 'Error' 2>/dev/null"
+
+# $password will be the master password;
+# make sure to use quotes around it
+list_command='$password_picker_dir/keepass-client -l $database_file "$password"'
+# $password will be the master password, $entry will be the name of the entry;
+# make sure to use quotes around them
+grab_command='$password_picker_dir/keepass-client $database_file "$password" -g "$entry"'
+
+password=$(eval $password_prompt_command)
+if [ -z "$password" ]; then
+    exit -1
+fi
+
+entries=$(eval $list_command)
+if [ -z "$entries" ]; then
+    eval $bad_password_alert
+    exit -1
+fi
+
+entry=$(echo "$entries" | dmenu -l 10)
+if [ -z "$entry" ]; then
+    exit -1
+fi
+
+eval $grab_command | xargs echo -n | xclip -selection c
